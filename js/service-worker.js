b5ac6651282195cf206c53840b1fe4acc3b38327
tomakela <teemu.o.makela@gmail.com>
2024-02-24 00:54:29 +0200

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,17 +1,18 @@
 // Choose a different app prefix name
 var APP_PREFIX = 'brcdgms_';
-var VERSION = 'version_03';
+var VERSION = 'version_04';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('precache-v1').then((cache) => {
       var lst = [
         '/BrcdGms/index.html',
         '/BrcdGms/style.css',
         '/BrcdGms/audio/cute-level-up-2-189851.mp3',
         '/BrcdGms/audio/menu-buttom-190020_cut.mp3',
         '/BrcdGms/audio/shush-90127.mp3',
-        '/BrcdGms/js/quagga.min.js'
+        '/BrcdGms/js/quagga.min.js',
+        '/BrcdGms/favicon.ico'
         // other assets you want to cache
       ];
 
