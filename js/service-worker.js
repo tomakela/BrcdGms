cec15de1bc736fe80804bd1acfa58e63681f6492
tomakela <teemu.o.makela@gmail.com>
2024-02-24 01:31:19 +0200

    res error

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,6 +1,6 @@
 // Choose a different app prefix name
 var APP_PREFIX = 'brcdgms_';
-var VERSION = 'version_06';
+var VERSION = 'version_08';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
