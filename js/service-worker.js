3549dd399cc1a18cc7fb092ab787c282a6b74cc5
tomakela <teemu.o.makela@gmail.com>
2024-02-24 01:07:50 +0200

    res

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,6 +1,6 @@
 // Choose a different app prefix name
 var APP_PREFIX = 'brcdgms_';
-var VERSION = 'version_04';
+var VERSION = 'version_05';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
